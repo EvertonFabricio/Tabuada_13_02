2299564c9f13d2e11adb5950be75f828e668303d
EvertonFabricio <everton.5by5@gmail.com>
2022-03-08 10:03:00 -0300

    Adicionar arquivos de projeto.

---
+++ Tabuada/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número de 1 a 9 para saber sua tabuada: ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nO numero digitado foi {num}. Sua tabuada é:");
+            int i = 1;
+
+            while (i <= 10)
+            {
+                Console.WriteLine($"{num}x{i} = {i * num}");
+                i++;
+            }
+        }
+    }
+}
